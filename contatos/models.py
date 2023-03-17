2294a2ea72d59a9773416319e842409c8bd950b3
Rafael Henrique <rafaelh9six@gmail.com>
2023-03-16 21:22:52 -0300

    criado o models do projeto

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    
+class Contato(models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=150)
+    telefone = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150, blank=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    
+    categoria = models.ForeignKey(Categoria)
