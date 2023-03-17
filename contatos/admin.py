02e255c0d37b8820056dc321f3dd15e4d2760464
Rafael Henrique <rafaelh9six@gmail.com>
2023-03-16 21:39:04 -0300

    exibindo os contatos no indes.html

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id' ,'nome', 'sobrenome', 'telefone' ,
+                    'email', 'data_criacao', 'categoria')
+    list_display_links = ('id' ,'nome', 'sobrenome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome',)
+    
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.nome
+    
+    
 class Contato(models.Model):
     nome = models.CharField(max_length=150)
-    sobrenome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=150, blank=True)
     telefone = models.CharField(max_length=150)
     email = models.EmailField(max_length=150, blank=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     
-    categoria = models.ForeignKey(Categoria)
+    def __str__(self):
+        return self.nome
