5cd24a4c7b1280725fe6022c51839506cbd22e23
Rafael Henrique <rafaelh9six@gmail.com>
2023-03-27 15:34:50 -0300

    correção feita na paginação,

--- contatos/views.py
+++ contatos/views.py
@@ -4,7 +4,6 @@ from .models import Contato
 from django.core.paginator import Paginator
 
 
-
 def index(request):
     contatos = Contato.objects.filter(mostrar=True)
     paginator = Paginator(contatos, 3)
